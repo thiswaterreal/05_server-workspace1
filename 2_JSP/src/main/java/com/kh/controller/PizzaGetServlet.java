c7f56e3d03a561e64f78426def85c0bcf78cf236
thiswaterreal <thiswaterreal@gmail.com>
2023-08-07 07:35:22 +0900

    commit

--- 2_JSP/src/main/java/com/kh/controller/PizzaGetServlet.java
+++ 2_JSP/src/main/java/com/kh/controller/PizzaGetServlet.java
@@ -32,18 +32,42 @@ public class PizzaGetServlet extends HttpServlet {
 		String address = request.getParameter("address");
 		String message = request.getParameter("message");
 		String pizza = request.getParameter("pizza");
-		String[] topping = request.getParameterValues("topping");
-		String[] side = request.getParameterValues("side");
+		String[] toppings = request.getParameterValues("topping");
+		String[] sides = request.getParameterValues("side");
 		String payment = request.getParameter("payment");
 		
 		System.out.println("userName : " + userName);
 		System.out.println("phone : " + phone);
 		System.out.println("address : " + address);
-		System.out.println("message : " + message);
+		//System.out.println("message : " + message);
 		System.out.println("pizza : " + pizza);
-		System.out.println("topping : " + String.join("/", topping));
-		System.out.println("side : " + String.join("/", side));
+		//System.out.println("toppings : " + String.join("/", toppings));
+		//System.out.println("sides : " + String.join("/", sides));
 		System.out.println("payment : " + payment);
+		
+		if(message.equals("")) { // 빈문자열이 넘어오는 경우
+			System.out.println("요청사항이 없습니다.");
+		}else {
+			System.out.println("message : " + message);			
+		}
+		
+		System.out.println("pizza : " + pizza);
+		
+		if(toppings == null) { // 아무런 선택도 안한 경우
+			System.out.println("topping : 없음");
+		}else { // 하나라도 선택한 경우
+			System.out.println("topping : " + String.join("/", toppings));
+		}
+		
+		if(sides == null) { // 아무런 선택도 안한 경우
+			System.out.println("side : 없음");
+		}else { // 하나라도 선택한 경우
+			System.out.println("side : " + String.join("/", sides));
+		}
+		
+		
+		
+		
 		
 	}
 
